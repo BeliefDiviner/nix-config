78e9edfddc9f0ff12fe36f714152e108989169e4
Fedor Fedorenko <41856345+BeliefDiviner@users.noreply.github.com>
2026-05-01 19:56:23 +0400

    feat: parametrising constants (#2)
    * parametrise constants
    
    * lock file

--- flake.nix
+++ flake.nix
@@ -1,21 +1,35 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, nixos-wsl, ... } @ inputs: {
-    nixosConfigurations = {
-      st-m5-wsl = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        system = "x86_64-linux";
+  outputs = { self, nixpkgs, nixpkgs-stable, nixos-wsl, ... } @ inputs:
+    let
+      machineSpecificValue = {
+        userName = "bd00ff";
+	hostName = "st-m5-wsl";
+        systemArch = "x86_64-linux";
+	isWSL = true;
+      };
+      mkNixosConfiguration = machineSpecific: nixpkgs.lib.nixosSystem {
+        system = machineSpecific.systemArch;
         modules = [
           nixos-wsl.nixosModules.default
-	  ./host/st-m5-wsl/configuration.nix
+          ./host/${machineSpecific.hostName}/configuration.nix
+	  ({ lib, ... }: {
+            options.machineSpecific = lib.mkOption {
+	      type = lib.types.attrs;
+              default = machineSpecific;
+	    };
+	  })
         ];
       };
+    in {
+      nixosConfigurations.${machineSpecificValue.hostName} = mkNixosConfiguration (
+        machineSpecificValue
+      );
     };
-  };
 }
 
