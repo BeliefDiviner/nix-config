1661f26b1912b8d4e80112af917971d8addd5b19
BeliefDiviner <41856345+BeliefDiviner@users.noreply.github.com>
2026-05-04 06:21:50 +0000

    fix ssh agent

--- hosts/st-m5-wsl/configuration.nix
+++ hosts/st-m5-wsl/configuration.nix
@@ -16,10 +16,14 @@
     enable = config.machineSpecific.isWSL;
     defaultUser = config.machineSpecific.userName;
     useWindowsDriver = true;
+    ssh-agent = {
+      enable = true;
+      users = [ config.machineSpecific.userName ];
+    };
   };
   environment.extraInit = ''
     export NIX_LD_LIBRARY_PATH="/usr/lib/wsl/lib:$NIX_LD_LIBRARY_PATH"
-  '';
+  '';  # wsl/lib is in path, but not when using nix-ld
 
   environment.systemPackages = with pkgs; [
     pkgs.zsh
@@ -32,10 +36,7 @@
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
 
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-  };
+  programs.ssh.startAgent = true;
 
   programs.nix-ld = {
     enable = true;
@@ -43,5 +44,10 @@
     ];
   };
 
-  system.stateVersion = "25.05"; # Did you read the comment?
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  system.stateVersion = "25.05";
 }
